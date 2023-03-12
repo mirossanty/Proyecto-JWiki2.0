31915293447a7bbe0c2824c3102f2d2cee72d045
JossLMont20 <120143222+JossLMont20@users.noreply.github.com>
2023-03-12 17:17:50 -0600

    Validacion de formulario de contacto

--- contacto.php
+++ contacto.php
@@ -36,26 +36,36 @@
                 <p class="mx-auto text-center texto">Agradecemos que haya visitado JWiki y que  quiera ponerse en contacto con nosotros. Puede utilizar el siguiente formulario para env&iacute;ar su mensaje y nosotros lo atenderemos a la brevedad respondiendo al correo electr&oacute;nico introducido en el formulario.</p>
             </div>
             <hr>
-            <form action="https://formspree.io/f/xqkjwrby" id="form" method="POST">
+            <form action="https://formspree.io/f/xqkjwrby" class="needs-validation" id="form" method="POST" novalidate>
+            <div class="form-row">
                 <div class="row">
                     <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text caja-form texto" id="basic-addon1">Nombre</span>
                             </div>
-                            <input type="text" class="form-control texto" id="name" placeholder="Nombre completo" name="name" required>
+                            <input type="text" class="form-control texto" id="name" placeholder="Nombre completo" name="name" pattern="[A-Za-zÁÉÍÓÚñáéíóú\s]{2,80}" required>
+                            <div class="invalid-feedback">
+                              El valor introducido no es valido, por favor introduce un valor valido
+                            </div>
                         </div>
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
                                 <span class="input-group-text caja-form texto" id="basic-addon2">Correo electr&oacute;nico</span>
                             </div>
-                            <input type="text" class="form-control texto" id="email" placeholder="Correo electr&oacute;nico" name="_replyto" required>
+                            <input type="email" class="form-control texto" id="email" placeholder="Correo electr&oacute;nico" name="_replyto" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+                            <div class="invalid-feedback">
+                              El valor introducido no es valido, por favor introduce un valor valido
+                            </div>
                         </div>
                         <div class="input-group">
                             <div class="input-group-prepend">
                                 <span class="input-group-text caja-form texto">Mensaje</span>
                             </div>
-                            <textarea class="form-control texto" id="msj" placeholder="Escribe tu mensaje" name="message" required></textarea>
+                            <textarea class="form-control texto" id="msj" placeholder="Escribe tu mensaje" name="message" pattern="[A-Za-zÁÉÍÓÚñáéíóú0-9\s.,!?()_-]{10,500}" required></textarea>
+                            <div class="invalid-feedback">
+                              El valor introducido no es valido, por favor introduce un valor valido
+                            </div>
                         </div>
                         <br>
                         <div>
@@ -68,7 +78,8 @@
                             <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15174.00518360293!2d-94.4017236!3d18.0483335!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfacfdae8bf7da4c1!2sUniversidad%20Tecnol%C3%B3gica%20del%20Sureste%20de%20Veracruz!5e0!3m2!1ses!2smx!4v1668303458058!5m2!1ses!2smx" width="690" height="180" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div> 
                     </div>                  
-                </div>    
+                </div>
+              </div>   
             </form>
         </div>
     </main>
@@ -134,6 +145,26 @@
   <!-- Footer -->
 
 </div>
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="js/bootstrap.min.js"></script>
